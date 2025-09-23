289170b9cb3ef6555a2a2dba015399208172d5e6
Vivek Kumar <vivekkumar.pro96@gmail.com>
2025-09-23 09:28:35 +0000

    Fix linting errors in Project 3

--- 3. Guessing Game/guessing_game/src/main.rs
+++ 3. Guessing Game/guessing_game/src/main.rs
@@ -1,43 +1,38 @@
-use std::io;
+use colored::{self, Colorize};
 use rand::Rng;
 use std::cmp::Ordering;
-use colored::{self, Colorize};
+use std::io;
 
 fn main() {
     println!("Guess The Number!");
 
-    let secret_number=rand::rng().random_range(1..=100);
+    let secret_number = rand::rng().random_range(1..=100);
     println!("Secret Number is: {}", secret_number);
 
     loop {
-        
         println!("Please input your guess!");
         let mut guess = String::new();
-        
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line!");
 
-        let guess: u32= match guess.trim().parse() {
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
                 println!("Please enter a valid number!");
                 continue;
-                }
-            };
+            }
+        };
 
-        println!("You Guessed: {}",guess);
+        println!("You Guessed: {}", guess);
 
-        match guess.cmp(&secret_number) 
-        {
-            Ordering::Less => println!("{}","Too Small!".red()),
-            Ordering::Greater => println!("{}","Too Big!".red()),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too Small!".red()),
+            Ordering::Greater => println!("{}", "Too Big!".red()),
             Ordering::Equal => {
-                println!("{}","You Win!".green());
+                println!("{}", "You Win!".green());
                 break;
             }
-        
         }
-    
     }
 }
